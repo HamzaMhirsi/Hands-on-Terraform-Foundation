73e8b6acd6c99cab67eae9e3a9cae12184625d58
Hamza M'hirsi <42467250+HamzaMhirsi@users.noreply.github.com>
2024-05-18 17:24:56 +0200

    Create datasources.tf

---
+++ datasources.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "server_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20240423"]
+  }
+}
